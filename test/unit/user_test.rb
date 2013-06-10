e0105791cf40e005d00f95a91c9c8914201211d2
Jeff Carlson <carlsonjf@gmail.com>
2013-06-10 19:41:16 -0400

    added unit test for profile_name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -27,10 +27,21 @@ class UserTest < ActiveSupport::TestCase
 	end	
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(:first_name => 'jeff', :last_name => 'carlson', :email => "jfc@gmail.com")
+		user.password = user.password_confirmation = '12345678'
+		
 		user.profile_name ="My Profile Name"
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(:first_name => 'jeff', :last_name => 'carlson', :email => "jfc@gmail.com")
+		user.password = user.password_confirmation = '12345678'
+		
+		user.profile_name = 'jfcarlson_1'
+		assert user.valid?
+
 	end
 end
